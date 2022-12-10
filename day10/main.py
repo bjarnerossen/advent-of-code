71ad723bc80b65880d5a90bc4ef2233c587bcf8e
Bjarne <92723881+bjarnerossen@users.noreply.github.com>
2022-12-11 00:44:21 +1300

    Cleanup

--- day10/main.py
+++ day10/main.py
@@ -3,66 +3,42 @@ with open("input.txt") as f:
     signals = f.read().splitlines()
 
     x = 1
-    cycles = {"20": 0, "60": 0, "100": 0, "140": 0, "180": 0, "220": 0}
-    cycle = 0 
+    cycles = (20, 60, 100, 140, 180, 220)
+    cycle = part1 = 0 
 
-    for signal in signals:
-        if signal != "noop":
-            for i in range(2):
-                cycle += 1
-                match cycle:
-                     case 20:
-                         cycles["20"] += (x * 20)
-                     case 60:
-                         cycles["60"] += (x * 60)
-                     case 100:
-                         cycles["100"] += (x * 100)
-                     case 140:
-                         cycles["140"] += (x * 140)
-                     case 180:
-                         cycles["180"] += (x * 180)
-                     case 220:
-                         cycles["220"] += (x * 220)
-                if i == 1:
-                    _, val = signal.split()
-                    x += int(val)
-
-            
+    for signal in signals:  
         if signal.startswith("noop"):
             cycle += 1
-            match cycle:
-                   case 20:
-                       cycles["20"] += (x * 20)
-                   case 60:
-                       cycles["60"] += (x * 60)
-                   case 100:
-                       cycles["100"] += (x * 100)
-                   case 140:
-                       cycles["140"] += (x * 140)
-                   case 180:
-                       cycles["180"] += (x * 180)
-                   case 220:
-                       cycles["220"] += (x * 220)
+            if cycle in cycles:
+                part1 += cycle * x
 
-print("Part1 Answer:", sum(value for value in cycles.values()))
+        else:
+            for i in range(2):
+                   cycle += 1
+                   if cycle in cycles:
+                       part1 += cycle * x
+                   if i == 1:
+                       x += int(signal.split()[1])
+
+print("Part1 Answer:", part1)
 
 # Part2
 crt = ""
 cycle = 1
-value = 1
+x = 1
 
 for signal in signals:
     if (cycle - 1) % 40 == 0:
         crt += "\n"
-    crt += "#" if (cycle - 1) % 40 in (value - 1, value, value + 1) else " "
+    crt += "#" if (cycle - 1) % 40 in (x - 1, x, x + 1) else " "
 
     if signal.startswith("noop"):
         cycle += 1
     else:
         if cycle % 40 == 0:
             crt += "\n"
-        crt += "#" if cycle % 40 in (value - 1, value, value + 1) else " "
+        crt += "#" if cycle % 40 in (x - 1, x, x + 1) else " "
         cycle += 2
-        value += int(signal.split()[1])
+        x += int(signal.split()[1])
 
 print("Part2 Answer:",crt)
