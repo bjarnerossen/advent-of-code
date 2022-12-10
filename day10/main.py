1c5d5a1846d31603e4d99acd89e660f42e3851a5
Bjarne <92723881+bjarnerossen@users.noreply.github.com>
2022-12-11 00:29:53 +1300

    part2 added

--- day10/main.py
+++ day10/main.py
@@ -1,15 +1,15 @@
+# Part1
 with open("input.txt") as f:
     signals = f.read().splitlines()
 
     x = 1
     cycles = {"20": 0, "60": 0, "100": 0, "140": 0, "180": 0, "220": 0}
-
     cycle = 0 
+
     for signal in signals:
         if signal != "noop":
             for i in range(2):
                 cycle += 1
-                # print(f"cycle:{cycle}, val:{x}")
                 match cycle:
                      case 20:
                          cycles["20"] += (x * 20)
@@ -28,7 +28,7 @@ with open("input.txt") as f:
                     x += int(val)
 
             
-        if signal == "noop":
+        if signal.startswith("noop"):
             cycle += 1
             match cycle:
                    case 20:
@@ -44,7 +44,25 @@ with open("input.txt") as f:
                    case 220:
                        cycles["220"] += (x * 220)
 
-        # print(f"cycle:{cycle}, val:{x}")
-
-# print(cycles)
 print("Part1 Answer:", sum(value for value in cycles.values()))
+
+# Part2
+crt = ""
+cycle = 1
+value = 1
+
+for signal in signals:
+    if (cycle - 1) % 40 == 0:
+        crt += "\n"
+    crt += "#" if (cycle - 1) % 40 in (value - 1, value, value + 1) else " "
+
+    if signal.startswith("noop"):
+        cycle += 1
+    else:
+        if cycle % 40 == 0:
+            crt += "\n"
+        crt += "#" if cycle % 40 in (value - 1, value, value + 1) else " "
+        cycle += 2
+        value += int(signal.split()[1])
+
+print("Part2 Answer:",crt)
