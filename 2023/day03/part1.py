540bad08c07cbbd49c8dabcdb2b9918738d5afbc
Bjarne <92723881+bjarnerossen@users.noreply.github.com>
2023-12-09 10:24:35 -0800

    day03 part2 code progress

--- 2023/day03/part1.py
+++ 2023/day03/part1.py
@@ -1,39 +1,37 @@
 import re
 from collections import defaultdict
 
-with open("test.txt") as file:
-    data = file.readlines()
+with open("input.txt") as file:
+    data = file.read().split("\n")
 
 # Finding coordinates for the symbols
 symbols = dict()
-for row, line in enumerate(data):
-        for col, char in enumerate(line):
+for y, line in enumerate(data):
+        for x, char in enumerate(line):
             if char not in "1234567890.":
-                symbols[(row, col)] = char
+                symbols[(x, y)] = char
     
-print(symbols)
+# print(symbols)
 
 # checking if a number has a neighboor containing a symbol
-gears = defaultdict(list)
 part_numbers_sum = 0
-for row, line in enumerate(data):
+for y, line in enumerate(data):
     for match in re.finditer(r"\d+", line):
         # print(match)
-        for (s_row, s_col), char in symbols.items():
-            # print(s_row, s_col, char)
+        for (s_x, s_y), char in symbols.items():
             # checking the start and end coordinates of each number
             start_position = match.start() - 1
             end_position = match.end()
-            is_within_row_range = start_position <= s_row <= end_position
+            is_within_row_range = start_position <= s_x <= end_position
 
             # checking the columns above and below each number
-            col_lower_bound = col - 1
-            col_upper_bound = col + 1
-            is_within_col_range = col_lower_bound <= s_col <= col_upper_bound
+            col_lower_bound = y - 1
+            col_upper_bound = y + 1
+            is_within_col_range = col_lower_bound <= s_y <= col_upper_bound
 
             if is_within_row_range and is_within_col_range:
                 num = int(match.group())
                 part_numbers_sum += num
                 break
 
-print(f"Sum of part numbers: {part_numbers_sum}")
+print(part_numbers_sum)
---
+++ 2023/day03/part2.py
@@ -0,0 +1,44 @@
+import re
+from collections import defaultdict
+from math import prod
+
+with open("input.txt") as file:
+    data = file.read().split("\n")
+
+# Finding coordinates for the symbols
+symbols = dict()
+for y, line in enumerate(data):
+        for x, char in enumerate(line):
+            if char not in "1234567890.":
+                symbols[(x, y)] = char
+
+
+# checking if a number has a neighboor containing a symbol
+gears = defaultdict(list)
+part_numbers_sum = 0
+for y, line in enumerate(data):
+    for match in re.finditer(r"\d+", line):
+        # print(match)
+        for (s_x, s_y), char in symbols.items():
+            # checking the start and end coordinates of each number
+            start_position = match.start() - 1
+            end_position = match.end()
+            is_within_row_range = start_position <= s_x <= end_position
+
+            # checking the columns above and below each number
+            col_lower_bound = y - 1
+            col_upper_bound = y + 1
+            is_within_col_range = col_lower_bound <= s_y <= col_upper_bound
+
+            if is_within_row_range and is_within_col_range:
+                num = int(match.group())
+                part_numbers_sum += num
+                if char == "*":
+                    gears[(s_x, s_y)].append(num)
+                break
+
+for part_nums in gears.values():
+    if len(part_nums) == 2:
+        print(sum(prod(part_nums)))
+
+print(gears)
