e421394bf856fd0b2057962ddd58bb757a6110f8
Bjarne <92723881+bjarnerossen@users.noreply.github.com>
2023-12-09 09:44:01 -0800

    part1 day3 first try

--- 2023/day03/part1.py
+++ 2023/day03/part1.py
@@ -1,42 +1,39 @@
-def main():
-    with open("test.txt") as file:
-        data = file.readlines()
+import re
+from collections import defaultdict
 
-    result = sum_part_numbers(data)
+with open("test.txt") as file:
+    data = file.readlines()
 
-def is_part_number(data, row, col):
-
-        symbols = "*+#$@&" 
-        neighbors = [
-            (-1, -1), (-1, 0), 
-            (-1, 1), (0, -1), 
-            (0, 1), (1, -1), 
-            (1, 0), (1, 1)
-        ]
-
-        if not data[row][col].isdigit():
-            return False
-        
-        for dr, dc in neighbors:
-            new_row, new_col = row + dr, col + dc
-            if (
-                0 <= new_row < len(data) and
-                0 <= new_col < len(data[0]) and
-                data[new_row][new_col] in symbols
-            ):
-                return True
-
-        return False
-
-def sum_part_numbers(data):
-        for row in range(len(data)):
-            for col in range(len(data[row])):
-                if is_part_number(data, row, col):
-                    total += int(data[row][col])
-        return total
-
+# Finding coordinates for the symbols
+symbols = dict()
+for row, line in enumerate(data):
+        for col, char in enumerate(line):
+            if char not in "1234567890.":
+                symbols[(row, col)] = char
     
+print(symbols)
 
+# checking if a number has a neighboor containing a symbol
+gears = defaultdict(list)
+part_numbers_sum = 0
+for row, line in enumerate(data):
+    for match in re.finditer(r"\d+", line):
+        # print(match)
+        for (s_row, s_col), char in symbols.items():
+            # print(s_row, s_col, char)
+            # checking the start and end coordinates of each number
+            start_position = match.start() - 1
+            end_position = match.end()
+            is_within_row_range = start_position <= s_row <= end_position
 
-if __name__ == "__main__":
-    main()
+            # checking the columns above and below each number
+            col_lower_bound = col - 1
+            col_upper_bound = col + 1
+            is_within_col_range = col_lower_bound <= s_col <= col_upper_bound
+
+            if is_within_row_range and is_within_col_range:
+                num = int(match.group())
+                part_numbers_sum += num
+                break
+
+print(f"Sum of part numbers: {part_numbers_sum}")
