cd0d33d566319f9ddf04634b07e3c3a671189733
Bjarne <92723881+bjarnerossen@users.noreply.github.com>
2023-12-15 21:21:26 -0800

    minor naming fixes

--- 2023/day06/part1.py
+++ 2023/day06/part1.py
@@ -5,7 +5,7 @@ data = open("input.txt").read().splitlines()
 race_durations = [int(ms) for ms in re.findall(r'\d+', data[0])]
 record_distances = [int(mm) for mm in re.findall(r'\d+', data[1])]
 
-def calculate_winning_strategies(race_duration, record_distance):
+def calculate_winning_options(race_duration, record_distance):
     wins = 0
     for hold_duration in range(1, race_duration):
         speed = hold_duration
@@ -14,7 +14,7 @@ def calculate_winning_strategies(race_duration, record_distance):
             wins += 1
     return wins
 
-winning_options = [calculate_winning_strategies(race_duration, record_distance)
+winning_options = [calculate_winning_options(race_duration, record_distance)
                     for race_duration, record_distance in zip(race_durations, record_distances)]
 
 print(math.prod(winning_options))
--- 2023/day06/part2.py
+++ 2023/day06/part2.py
@@ -1,16 +1,18 @@
-import math
 import re
 
 data = open('input.txt').read().replace(' ', '')
 race_duration = int(re.findall(r'\d+', data)[0])
 record_distance = int(re.findall(r'\d+', data)[1])
 
-wins = 0
-for hold_duration in range(14, race_duration-13):
-    speed = hold_duration
-    distance = (race_duration - hold_duration) * speed
-    if distance > record_distance:
-        wins += 1
+def calculate_winning_options(race_duration, record_distance):
+    wins = 0
+    for hold_duration in range(1, race_duration):
+        speed = hold_duration
+        distance = (race_duration - hold_duration) * speed
+        if distance > record_distance:
+            wins += 1
+    return wins
 
+winning_options = calculate_winning_options(race_duration, record_distance)
 
-print(wins)
+print(winning_options)
