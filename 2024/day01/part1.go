4f84264dfa5a46b5a74ebd6533dcb2593a3ae831
bjarnerossen <92723881+bjarnerossen@users.noreply.github.com>
2024-12-02 02:58:58 +0000

    part1 day01

--- 2024/day01/part1.go
+++ 2024/day01/part1.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	content, err := os.ReadFile("input.txt")
+	var left []int
+	var right []int
+
+	input, err := os.ReadFile("input.txt") // Read input file
 	if err != nil {
 		fmt.Println("Error reading file:", err)
 		return
 	}
-	fmt.Println(string(content))
-}
+
+	scanner := bufio.NewScanner(bytes.NewReader(input)) // Create a scanner to read from the input
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Split(line, "   ")
+		fmt.Println(parts[0])
+		}
+	}
