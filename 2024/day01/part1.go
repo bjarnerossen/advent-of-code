ee05e96f5421352824a634b1a00f5ad8de03d77a
bjarnerossen <92723881+bjarnerossen@users.noreply.github.com>
2024-12-02 03:17:35 +0000

    part1 day01 done

--- 2024/day01/part1.go
+++ 2024/day01/part1.go
@@ -5,11 +5,13 @@ import (
 	"bytes"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
 
 func main() {
+	var answer int
 	var left []int
 	var right []int
 
@@ -22,7 +24,36 @@ func main() {
 	scanner := bufio.NewScanner(bytes.NewReader(input)) // Create a scanner to read from the input
 	for scanner.Scan() {
 		line := scanner.Text()
-		parts := strings.Split(line, "   ")
-		fmt.Println(parts[0])
+		parts := strings.Fields(line)
+
+		if len(parts) == 2 {
+			l, err := strconv.Atoi(parts[0])
+			if err != nil {
+				fmt.Println("Error converting left part:", err)
+				continue
+			}
+			r, err := strconv.Atoi(parts[1])
+			if err != nil {
+				fmt.Println("Error converting right part:", err)
+				continue
+			}
+
+			// Append to the left and right slices
+			left = append(left, l)
+			right = append(right, r)
 		}
 	}
+	sort.Ints(left)
+	sort.Ints(right)
+
+	if len(left) != len(right) {
+		fmt.Println("Slices are not of equal length")
+		return
+	}
+
+	for i := 0; i < len(left); i++ {
+		diff := right[i] - left[i]
+		answer += diff
+	}
+	fmt.Println(answer)
+}
